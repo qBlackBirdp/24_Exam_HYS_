c363148dd8446daf9eee9f58ea79baa80008d75b
qBlackbirdp <blackbird9711@gmail.com>
2024-07-25 17:00:44 +0900

    2번 문제

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,7 +1,20 @@
 package org.koreait;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = x - i; j > 0; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
     }
 }
