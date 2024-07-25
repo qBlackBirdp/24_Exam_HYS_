946beef2309cfcc1c2d7154e9514948ce8b4f5a9
qBlackbirdp <blackbird9711@gmail.com>
2024-07-25 17:02:46 +0900

    1번 문제

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -6,13 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
+        int y = sc.nextInt();
 
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = x - i; j > 0; j--) {
-                System.out.print(j + " ");
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
